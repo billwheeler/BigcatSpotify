7462181c0beb4ff8bf146b648344150c841377c5
Bill Wheeler <bwheeler@mathway.com>
2019-06-12 12:01:04 -0400

    trying out axios as the ajax lib

--- js/src/main.js
+++ js/src/main.js
@@ -1,9 +1,17 @@
 ﻿"use strict"
 
+var axios = require('axios')
+
 global.Debug = require("./utils/debug.js");
 
 module.exports = {
     run: () => {
-        Debug.log("am big cat.")
+        axios.get('package.json')
+            .then(function (response) {
+                Debug.log("big cat make ajax", response)
+            })
+            .catch(function (error) {
+                Debug.log("uh oh, bad big cat", error);
+            })
     }
 }
